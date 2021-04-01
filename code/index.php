1ff7c41b89abdba5b5fa98eb7ff15a69d17e1b53
Nurzamal <n.zaman98@gmail.com>
2021-04-01 15:32:27 +0530

    Form submission completed-3rd commit

--- code/index.php
+++ code/index.php
@@ -1,36 +1,44 @@
 <?php
+
+$insert = false;
+
 if(isset($_POST['name'])){
 
+    //Set connection variables
     $server = "localhost:3307";
     $username = "root";
     $password = "";
     $db = "trip";
 
-    $con = mysqli_connect($server, $username, $password, $db);
+    $con = mysqli_connect($server, $username, $password, $db); //creating database connections
 
+    //check for connection error and giving message
     if(!$con){
         die("Failed to connect-").
         mysqli_connect_error();
     }
+    
     // echo "Connected to database succesfully";
 
-
+    //collect data from html form
     $name = $_POST['name'];
     $age = $_POST['age'];
     $email = $_POST['email'];
     $phone = $_POST['phone'];
     $message = $_POST['message'];
 
+    //sql query
     $sql = "INSERT INTO `student` (`name`, `age`, `email`, `phone`, `message`, `dt`) VALUES ('$name', '$age', '$email', '$phone', '$message', current_timestamp());";
 
+    //execute the query if connected to db
     if($con->query($sql) == true){
-        echo "Data Submitted Successfully";
+        $insert = true;  //flag for successful insertion to db
     }
     else{
         echo "ERROR: $sql <br> $con->error";
     }
 
-    $con->close();
+    $con->close(); //close the database connection
 }
 ?>
 
@@ -47,10 +55,15 @@ if(isset($_POST['name'])){
 </head>
 <body>
 
+<div class = "banner">
+    <img src = "banner.jpg">
+</div>
+
     <div class="container">
 
-        <h2>Welcome to IITK Trip Form</h2>
+        <h2>Welcome to NIELIT Guwahati</h2>
         <p>Enter your details:</p>
+
 
         <form action="index.php" method="POST">
 
@@ -58,11 +71,17 @@ if(isset($_POST['name'])){
             <input type="text" name="age" id="age" placeholder="Your Age" required>
             <input type="email" name="email" id="email" placeholder="Your Email" required>
             <input type="text" name="phone" id="phone" placeholder="Your Phone No" required>
-            <textarea name="message" id="message" rows="5" cols = "20" placeholder="Write your message.."></textarea>
+            <textarea name="message" id="message" rows="5" cols = "20" placeholder="Write your message.."></textarea><br>
             <button class="btn">Submit</button>
 
 
         </form>
+
+        <?php
+            if($insert==true){
+                echo "<br><p class='submitMsg'> Form Submitted Successfully. </p>";
+            }
+        ?>
 
     </div>
     <script src = file.js></script>
